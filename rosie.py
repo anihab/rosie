0950d223ec35caf057753c9e080823bf6b8e6cf6
anisa <74878427+anihab@users.noreply.github.com>
2024-11-17 12:21:55 -0700

    add logging file

--- rosie.py
+++ rosie.py
@@ -1,14 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Main file for Rosie, a Discord bot dedicated to helping user's manage their time.
+
+This file serves as the entry point for the bot's functionality. It initializes the bot,
+loads extensions (cogs) for various features, and sets up global configurations.
+
+For more information, please refer to my github repo https://github.com/anihab/rosie
+or the discord.py documentation https://discordpy.readthedocs.io/en/stable/index.html.
+
+Author: anisa
+Version: 1.0
+"""
+
 import json
-import os
 import logging
+import os
 import sys
+
 from datetime import datetime, timezone
+from dotenv import load_dotenv
 
 import aiosqlite
 import discord
 from discord.ext import commands, tasks
 from discord.ext.commands import Context
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -18,15 +35,27 @@ if not os.path.isfile(config_path ):
 else:
     with open(config_path, "r") as config_file:
         config = json.load(config_file)
+        
+if not os.path.exists("logs"):
+    os.makedirs("logs")
 
-# TODO: set up logger file (remember to add it to .gitignore)
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+# File and console handler
+log_file = os.path.join("logs", "rosie.log")
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+    datefmt="%d/%b/%Y %H:%M:%S",
+    handlers=[
+        logging.FileHandler(log_file, encoding="utf-8", mode="w"),
+        logging.StreamHandler(),  # Log to console as well
+    ],
+)
 logger = logging.getLogger(__name__)
 
 intents = discord.Intents.default()
 
 class Rosie(commands.Bot):
-    def __init__(self) -> None:
+    def __init__(self) -> None: # `-> None` is for documentation purposes. This means the function does not return any value.
         super().__init__(
             command_prefix=commands.when_mentioned_or(config["prefix"]),
             intents=intents,
@@ -37,11 +66,11 @@ class Rosie(commands.Bot):
         self.config = config
        
     async def setup_hook(self) -> None:
-        await self.tree.sync(guild=discord.Object(int(os.getenv("GID"))))            
+        await self.tree.sync()            
         await self.init_db()
         await self.load_cogs()
         
-    async def on_ready(self):
+    async def on_ready(self)  -> None:
         logger.info("Logged on as %s!", self.user.name)
         try:
             # sync commands after the bot is ready
@@ -86,6 +115,26 @@ class Rosie(commands.Bot):
         if message.author == self.user or message.author.bot:
             return
         await self.process_commands(message)
+        
+    async def on_command_completion(self, context: Context) -> None:
+        self.logger.info("%s executed %s command", context.author, context.command.name)
+        
+    async def on_command_error(self, context, exception):
+        if isinstance(exception, commands.CommandOnCooldown):
+            embed = discord.Embed(
+                description="this command is currently on cooldown. you can try again in a little bit!",
+                color=0xf8eccf
+            )
+            await context.send(embed, ephemeral=True)
+        elif isinstance(exception, commands.NotOwner):
+            self.logger.warning("%s tried to execute an owner only command.", context.author)
+            embed = discord.Embed(
+                description="sorry, only the owner can execute this command!",
+                color=0xf8bdb9
+            )
+            await context.send(embed, ephemeral=True)
+        else:
+            return await super().on_command_error(context, exception)       
 
 rosie = Rosie()
 rosie.run(os.getenv("TOKEN"))
